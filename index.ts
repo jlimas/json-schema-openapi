9b793fabdc281c51d1a82b8515fbc0eb38c3582d
Jorge Limas <368854+jlimas@users.noreply.github.com>
2023-11-24 16:00:08 -0700

    Remove non-needed keys for openapi

--- index.ts
+++ index.ts
@@ -1,13 +1,26 @@
 import convert from "@openapi-contrib/json-schema-to-openapi-schema";
 import jsonToSchema from "json-schema-generator";
 
+const removeKeys = ["minLength", "uniqueItems", "minItems"];
+
+function cleanKeys(payload: any) {
+  Object.keys(payload).forEach((key) => {
+    if (removeKeys.includes(key)) {
+      delete payload[key];
+    } else if (typeof payload[key] === "object") {
+      cleanKeys(payload[key]);
+    }
+  });
+  return payload;
+}
+
 const server = Bun.serve({
   port: process.env.PORT || 3000,
   async fetch(request) {
     const body = await request.json();
     const jsonSchema = jsonToSchema(body);
     const openapiSchema = await convert(jsonSchema);
-    return Response.json(openapiSchema);
+    return Response.json(cleanKeys(openapiSchema));
   },
 });
 
